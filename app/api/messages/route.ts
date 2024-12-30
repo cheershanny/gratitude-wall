ac0b44005f261300cfa667e227f55cbf0ddd2465
Hanny Tran <cheershanny@gmail.com>
2024-12-30 22:29:24 +0700

    Add connection management

--- app/api/messages/route.ts
+++ app/api/messages/route.ts
@@ -1,19 +1,33 @@
 import { createMessageSchema } from "@/app/validationSchema";
 import { NextRequest, NextResponse } from "next/server";
-import client from "@/prisma/client";
+import prisma from "@/prisma/client";
 
 export async function POST(request: NextRequest) {
-  const body = await request.json();
-  const { message, name } = body;
-  const validation = createMessageSchema.safeParse(body);
-  if (!validation.success)
-    return NextResponse.json(validation.error.errors, { status: 400 });
-  
-  const newMessage = await client.message.create({
-    data: {
-      message, 
-      name, 
-    },
-  });
-  return NextResponse.json(newMessage, { status: 201 });
+  try {
+    const body = await request.json();
+    const { message, name } = body;
+    const validation = createMessageSchema.safeParse(body);
+    
+    if (!validation.success)
+      return NextResponse.json(validation.error.errors, { status: 400 });
+
+    await prisma.$connect();
+    
+    const newMessage = await prisma.message.create({
+      data: {
+        message, 
+        name, 
+      },
+    });
+
+    return NextResponse.json(newMessage, { status: 201 });
+  } catch (error) {
+    console.error('Error:', error);
+    return NextResponse.json(
+      { error: 'Internal Server Error' },
+      { status: 500 }
+    );
+  } finally {
+    await prisma.$disconnect();
+  }
 }
