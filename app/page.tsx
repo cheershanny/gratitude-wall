c4b300477bc783496bd962b853f33d28c94cd6d3
Hanny Tran <cheershanny@gmail.com>
2024-12-30 22:24:08 +0700

    update UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,18 @@
 import { Suspense } from "react";
 import MessageForm from "./components/MessageForm";
 import MessageList from "./components/MessageList";
+import Image from "next/image";
 export const dynamic = "force-dynamic";
 
 export default function Home() {
   return (
-    <div className="min-h-screen flex flex-col justify-center items-center p-4">
+    <div className="min-h-screen flex flex-col justify-center items-center p-4 bg-custom-bg">
+      <Image
+      src="/logo.png"
+      width={100}
+      height={100}
+      alt="logo"
+    />
       <h1 className="text-2xl font-bold mb-6">Share Your Gratitude</h1>
       <div className="w-full max-w-xl space-y-6">
         <MessageForm />
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "custom-bg": "#d5d2be",
+      },
+    },
   },
   daisyui: {
     themes: ["light", "dark", "pastel"],
