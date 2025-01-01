7b4ea4a8b2ca27ad1a5631394de59191597d2f6d
Hanny Tran <cheershanny@gmail.com>
2025-01-01 16:00:09 +0700

    remove try/catch and prisma disconnect

--- app/api/messages/route.ts
+++ app/api/messages/route.ts
@@ -3,31 +3,17 @@ import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/prisma/client";
 
 export async function POST(request: NextRequest) {
-  try {
-    const body = await request.json();
-    const { message, name } = body;
-    const validation = createMessageSchema.safeParse(body);
-    
-    if (!validation.success)
-      return NextResponse.json(validation.error.errors, { status: 400 });
-
-    await prisma.$connect();
-    
-    const newMessage = await prisma.message.create({
-      data: {
-        message, 
-        name, 
-      },
-    });
-
-    return NextResponse.json(newMessage, { status: 201 });
-  } catch (error) {
-    console.error('Error:', error);
-    return NextResponse.json(
-      { error: 'Internal Server Error' },
-      { status: 500 }
-    );
-  } finally {
-    await prisma.$disconnect();
-  }
+  const body = await request.json();
+  const { message, name } = body;
+  const validation = createMessageSchema.safeParse(body);
+  if (!validation.success)
+    return NextResponse.json(validation.error.errors, { status: 400 });
+  
+  const newMessage = await prisma.message.create({
+    data: {
+      message, 
+      name, 
+    },
+  });
+  return NextResponse.json(newMessage, { status: 201 });
 }
